246250bc431f678f00089e6c639c35cf9dfbc0c9
Daniel Skantze <danielskantze@gmail.com>
2025-09-17 07:44:23 +0200

    Add comment

--- src/render/shaders/accumulate.fs.glsl
+++ src/render/shaders/accumulate.fs.glsl
@@ -15,6 +15,7 @@ layout(location = 1) out vec4 out_updated;
 
 const float fade_time = 2.0;
 
+
 void main() {
   vec4 p_color = texelFetch(previous_color_tex, ivec2(gl_FragCoord.xy), 0);
   vec4 p_updated = texelFetch(previous_updated_tex, ivec2(gl_FragCoord.xy), 0);
@@ -24,6 +25,10 @@ void main() {
   float p_i = clamp(1.0 - (u_time - p_updated.x) / fade_time, 0.0, 1.0);
   p_i = smoothstep(0.0, 1.0, p_i);
   float c_i = u_time - updated.x;
+
+  // - New + old particle: premultiply alpha to determine the tone, then replace the alpha with 1.0 and write to output texture. New updated timestamp is written to updated texture. 
+  //- Old particles: alpha will correspond to the remaining life of that pixel, then write to output texture. Old updated timestamp is copied to updated texture. 
+
   out_color = c_i < 0.1 ? 
     p_color * vec4(1.0 - color.a) * p_i + vec4(color.rgb * color.a, 1.0) :
     p_color;
