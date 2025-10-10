47821835c178dd5660a73eb0dedc8078bed72ed9
Daniel Skantze <danielskantze@gmail.com>
2025-10-10 08:34:16 +0200

    Refactor rendering for realtime pipeline reconfiguration. Add use bloom and updates / draw parameters.

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import * as stage_simulate from "./render/stages/simulate";
 import { WebGLTextureError } from "./types/error";
 import { UniformComponents, type Uniform, type UniformType, type UniformUI } from "./types/gl/uniforms";
 import { type Settings } from "./types/settings";
+import type { Stage } from "./types/stage";
 import { createButtons } from "./ui/components/buttons";
 import { createCosPalette } from "./ui/components/cos-palette";
 import { createScalar } from "./ui/components/scalar";
@@ -20,196 +21,291 @@ import ControlFactory from "./ui/controls";
 export type ControlFactoryUniform = Omit<Uniform, "location" | "slot">;
 
 const settings: Settings = {
-    width: window.screen.width,
-    height: window.screen.height,
-    msaa: undefined
+  width: window.screen.width,
+  height: window.screen.height,
+  msaa: undefined
+}
+
+type RenderingStages = {
+  simulate: Stage,
+  materialize: Stage,
+  accumulate: Stage,
+  luma: Stage,
+  blur: Stage,
+  combine: Stage,
+  display: Stage
+};
+
+type RenderingConfig = {
+  maxNumParticles: number,
+  useBloom: boolean,
+  updatesPerDraw: number
 }
 
 function configureCanvas(canvas: HTMLCanvasElement) {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const dpr = window.devicePixelRatio;
-    canvas.width = width * dpr;
-    canvas.height = height * dpr;
-    canvas.style.width = `${width}px`;
-    canvas.style.height = `${height}px`;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const dpr = window.devicePixelRatio;
+  canvas.width = width * dpr;
+  canvas.height = height * dpr;
+  canvas.style.width = `${width}px`;
+  canvas.style.height = `${height}px`;
 }
 
 function createUniformControls(controlsContainer: HTMLElement, uniforms: ControlFactoryUniform[]) {
-    for (const u of uniforms) {
-        const { ui } = u;
-        if (ui) {
-            const { name, min, max, step, component } = u.ui!;
-            const numComponents = UniformComponents[u.type!]!;
-            if (component === "cos-palette") {
-              controlsContainer.appendChild(
-                createCosPalette(u.value as Matrix4x3)
-              )
-            } else if (numComponents > 1) {
-                const values = u.value as number[];
-                if (numComponents === 3) {
-                  controlsContainer.appendChild(
-                    createVec3(name, values as Vec3, 
-                      (v: Vec3) => { 
-                        values[0] = v[0]; 
-                        values[1] = v[1]; 
-                        values[2] = v[2]; 
-                      }
-                    )
-                  );
-                } else {
-                  controlsContainer.appendChild(createVector(name, values, (i: number, v: number) => { values[i] = v; }, min, max, step));
-                }
-            } else {
-                const value = u.value as number;
-                controlsContainer.appendChild(createScalar(name, value, (v: number) => { u.value = v; }, min, max, step));
-            }
+  for (const u of uniforms) {
+    const { ui } = u;
+    if (ui) {
+      const { name, min, max, step, component } = u.ui!;
+      const numComponents = UniformComponents[u.type!]!;
+      if (component === "cos-palette") {
+        controlsContainer.appendChild(
+          createCosPalette(u.value as Matrix4x3)
+        )
+      } else if (numComponents > 1) {
+        const values = u.value as number[];
+        if (numComponents === 3) {
+          controlsContainer.appendChild(
+            createVec3(name, values as Vec3,
+              (v: Vec3) => {
+                values[0] = v[0];
+                values[1] = v[1];
+                values[2] = v[2];
+              }
+            )
+          );
+        } else {
+          controlsContainer.appendChild(createVector(name, values, (i: number, v: number) => { values[i] = v; }, min, max, step));
         }
+      } else {
+        const value = u.value as number;
+        controlsContainer.appendChild(createScalar(name, value, (v: number) => { u.value = v; }, min, max, step));
+      }
     }
+  }
 }
 
 function textureSizeFromNumParticles(numParticles: number, maxNumParticles: number): [number, number] {
-    let pts = numParticles;
-    if (pts > maxNumParticles) {
-        pts = maxNumParticles;
-    }
-    const w = Math.floor(Math.sqrt(pts));
-    return [w, w];
+  let pts = numParticles;
+  if (pts > maxNumParticles) {
+    pts = maxNumParticles;
+  }
+  const w = Math.floor(Math.sqrt(pts));
+  return [w, w];
 }
 
 function createUi(
-    controlsContainer: HTMLElement,
-    parameters: ControlFactoryUniform[],
-    resetFn: () => void,
-    pauseFn: () => void,
-    toggleVisibilityFn: () => void,
+  controlsContainer: HTMLElement,
+  parameters: ControlFactoryUniform[],
+  resetFn: () => void,
+  pauseFn: () => void,
+  toggleVisibilityFn: () => void,
 ) {
-    createUniformControls(controlsContainer, parameters);
-    controlsContainer.appendChild(createButtons([
-        {title: "Clear", onClick: () => { resetFn(); }, color: 2},
-        {title: "Pause", onClick: pauseFn, color: 2}
-    ]));
+  createUniformControls(controlsContainer, parameters);
+  controlsContainer.appendChild(createButtons([
+    { title: "Clear", onClick: () => { resetFn(); }, color: 2 },
+    { title: "Pause", onClick: pauseFn, color: 2 }
+  ]));
 
-    document.addEventListener("keypress", (e: KeyboardEvent) => {
-      if (e.ctrlKey && e.key.charCodeAt(0) === 112) { // 112 = p
-        toggleVisibilityFn();
-      }
-    });
+  document.addEventListener("keypress", (e: KeyboardEvent) => {
+    if (e.ctrlKey && e.key.charCodeAt(0) === 112) { // 112 = p
+      toggleVisibilityFn();
+    }
+  });
 }
 
 function createUIParameter(type: UniformType, value: number | number[], ui: UniformUI): ControlFactoryUniform {
-    return { type, value, ui };
+  return { type, value, ui };
+}
+
+function createRenderingStages(gl: WebGL2RenderingContext, maxNumParticles: number, renderWidth: number, renderHeight: number): RenderingStages {
+  const simulate = stage_simulate.create(gl, maxNumParticles);
+  const materialize = stage_materialize.create(gl, simulate, renderWidth, renderHeight, maxNumParticles, settings.msaa);
+  const accumulate = stage_accumulate.create(gl, materialize);
+  const luma = stage_luma.create(gl, accumulate);
+  const blur = stage_blur.create(gl, luma, "high", 7);
+  const combine = stage_combine.create(gl, accumulate);
+  const display = stage_display.create(gl, combine);
+  return {
+    simulate, materialize, accumulate, luma, blur, combine, display
+  }
+}
+
+function configureRenderingStages(config: RenderingConfig, stages: RenderingStages) {
+  if (config.useBloom) {
+    stages.display.input = stages.combine;
+  } else {
+    stages.display.input = stages.accumulate;
+  }
+}
+
+type BloomStageParams = {
+  lumaThreshold: number,
+  bloomIntensity: number
+}
+
+type RenderingState = {
+  time: number,
+  frame: number,
+  width: number,
+  height: number,
+  numParticles: number,
+  stages: {
+    bloom: BloomStageParams
+  }
+}
+
+
+function drawRenderStages(gl: WebGL2RenderingContext, config: RenderingConfig, stages: RenderingStages, state: RenderingState) {
+  const { maxNumParticles, useBloom } = config;
+  const { time, frame, width, height, numParticles, stages: { bloom } } = state;
+  const drawSize = textureSizeFromNumParticles(numParticles, maxNumParticles);
+  stage_simulate.draw(gl, stages.simulate, time, frame, drawSize);
+  stage_materialize.draw(gl, stages.materialize, time, frame, numParticles);
+  stage_accumulate.draw(gl, stages.accumulate, time, frame);
+  if (useBloom) {
+    stage_luma.draw(gl, stages.luma, bloom.lumaThreshold);
+    stage_blur.draw(gl, stages.blur);
+    stage_combine.draw(gl, stages.combine, stages.blur, 1.0, bloom.bloomIntensity);
+  }
+  stage_display.draw(gl, stages.display, width, height);
+//  console.log(config, state, width, height);
 }
 
 function main(canvas: HTMLCanvasElement, controls: HTMLDivElement) {
+  configureCanvas(canvas);
+  let isRunning = true;
+  let elapsedTime = 0;
+  let startTime = performance.now();
+  const maxNumParticles = 4 * 50000;
+  let numParticlesParam = createUIParameter("int", 1000, {
+    name: "Particles",
+    min: 10,
+    max: maxNumParticles
+  });
+  let accumulateParam = createUIParameter("int", 1, {
+    name: "Accumulate",
+    min: 0,
+    step: 1,
+    max: 1
+  });
+  let bloomIntensityParam = createUIParameter("float", 0.5, {
+    name: "Bloom Intensity",
+    min: 0,
+    step: 0.01,
+    max: 1.0
+  });
+  let lumaThresholdParam = createUIParameter("float", 0.25, {
+    name: "Luma",
+    min: 0,
+    step: 0.01,
+    max: 1.0
+  });
+  let useBloomParam = createUIParameter("int", 0, {
+    name: "Bloom",
+    min: 0,
+    max: 1,
+    step: 1
+  });
+  let updatesPerDrawParam = createUIParameter("int", 4, {
+    name: "Updates / Draw",
+    min: 1,
+    max: 10,
+    step: 1
+  });
+  const controlFactory = new ControlFactory(controls);
+  const gl = canvas.getContext("webgl2")!
+  let ext = gl.getExtension("EXT_color_buffer_float");
+  if (!ext) {
+    throw new WebGLTextureError("This browser does not support rendering to float textures");
+  }
+  ext = gl.getExtension("EXT_color_buffer_half_float");
+  if (!ext) {
+    throw new WebGLTextureError("This browser does not support rendering to half float textures");
+  }
+  //const simulateStage = stage_simulate.create(gl, maxNumParticles);
+  //const testStage = stage_test.create(gl, canvas.width, canvas.height);
+  const dpr = window.devicePixelRatio;
+  const renderWidth = settings.width * dpr;
+  const renderHeight = settings.height * dpr;
+
+  const stages = createRenderingStages(gl, maxNumParticles, renderWidth, renderHeight);
+
+  let frame = 0;
+
+  const renderConfig: RenderingConfig = {
+    maxNumParticles,
+    useBloom: (useBloomParam.value as number) === 1,
+    updatesPerDraw: 4
+  };
+
+  configureRenderingStages(renderConfig, stages);
+
+  function draw() {
+    if (!isRunning) {
+      return;
+    }
+
+    const useBloom = (useBloomParam.value as number) === 1;
+    if (useBloom !== renderConfig.useBloom) {
+      renderConfig.useBloom = useBloom;
+      configureRenderingStages(renderConfig, stages);
+    }
+    renderConfig.updatesPerDraw = updatesPerDrawParam.value as number;
+
+    const renderingState: RenderingState = {
+      time: elapsedTime + (performance.now() - startTime) / 1000,
+      frame,
+      numParticles: numParticlesParam.value as number,
+      stages: {
+        bloom: {
+          lumaThreshold: lumaThresholdParam.value as number,
+          bloomIntensity: bloomIntensityParam.value as number
+        }
+      },
+      width: canvas.width,
+      height: canvas.height
+    };
+    for (let i = 0; i < renderConfig.updatesPerDraw; i++) {
+      renderingState.frame = frame;
+      drawRenderStages(gl, renderConfig, stages, renderingState);
+      frame++;
+    }
+    requestAnimationFrame(() => {
+      draw();
+    });
+  }
+
+  function resize() {
     configureCanvas(canvas);
-    let isRunning = true;
-    let elapsedTime = 0;
-    let startTime = performance.now();
-    const maxNumParticles = 4 * 50000;
-    let numParticlesParam = createUIParameter("int", 1000, {
-        name: "Particles",
-        min: 10,
-        max: maxNumParticles
-    });
-    let accumulateParam = createUIParameter("int", 1, {
-        name: "Accumulate",
-        min: 0,
-        step: 1,
-        max: 1
-    });
-    let bloomIntensityParam = createUIParameter("float", 0.5, {
-        name: "Bloom",
-        min: 0,
-        step: 0.01,
-        max: 1.0
-    });
-    let lumaThresholdParam = createUIParameter("float", 0.25, {
-        name: "Luma",
-        min: 0,
-        step: 0.01,
-        max: 1.0
-    });    
-    const controlFactory = new ControlFactory(controls);
-    const gl = canvas.getContext("webgl2")!
-    let ext = gl.getExtension("EXT_color_buffer_float");
-    if (!ext) {
-      throw new WebGLTextureError("This browser does not support rendering to float textures");
+  }
+  createUi(controls, [numParticlesParam, accumulateParam, updatesPerDrawParam, useBloomParam, bloomIntensityParam, lumaThresholdParam, ...stages.simulate.parameters],
+    () => { resize(); },
+    () => {
+      isRunning = !isRunning;
+      if (!isRunning) {
+        elapsedTime += (performance.now() - startTime) / 1000;
+      } else {
+        startTime = performance.now();
+        draw();
+      }
+    },
+    () => {
+      controlFactory.visible = !controlFactory.visible;
     }
-    ext = gl.getExtension("EXT_color_buffer_half_float");
-    if (!ext) {
-      throw new WebGLTextureError("This browser does not support rendering to half float textures");
-    }
-    const simulateStage = stage_simulate.create(gl, maxNumParticles);
-    //const testStage = stage_test.create(gl, canvas.width, canvas.height);
-    const dpr = window.devicePixelRatio;
-    const renderWidth = settings.width * dpr;
-    const renderHeight = settings.height * dpr;
+  );
 
-
-    const materializeStage = stage_materialize.create(gl, simulateStage, renderWidth, renderHeight, maxNumParticles, settings.msaa);
-    const accumulateStage = stage_accumulate.create(gl, materializeStage);
-    const lumaStage = stage_luma.create(gl, accumulateStage);
-    const blurStage = stage_blur.create(gl, lumaStage, "high", 7);
-    const combineStage = stage_combine.create(gl, accumulateStage);
-    const displayStage = stage_display.create(gl, combineStage);
-    let frame = 0;
-
-    function draw() {
-        if (!isRunning) {
-            return;
-        }
-
-        const time = elapsedTime + (performance.now() - startTime) / 1000;
-        for (let i = 0; i < 4; i++) {
-            const numParticles = numParticlesParam.value as number;
-            const drawSize = textureSizeFromNumParticles(numParticles, maxNumParticles);
-            stage_simulate.draw(gl, simulateStage, time, frame, drawSize);
-            stage_materialize.draw(gl, materializeStage, time, frame, numParticles);
-            stage_accumulate.draw(gl, accumulateStage, time, frame);
-            stage_luma.draw(gl, lumaStage, lumaThresholdParam.value as number);
-            stage_blur.draw(gl, blurStage);
-            stage_combine.draw(gl, combineStage, blurStage, 1.0, bloomIntensityParam.value as number);
-            stage_display.draw(gl, displayStage, canvas.width, canvas.height);
-            frame++;
-        }
-        requestAnimationFrame(() => {
-            draw();
-        });
-    }
-
-    function resize() {
-        configureCanvas(canvas);
-    }
-    createUi(controls, [numParticlesParam, accumulateParam, bloomIntensityParam, lumaThresholdParam, ...simulateStage.parameters],
-        () => { resize(); },
-        () => {
-            isRunning = !isRunning;
-            if (!isRunning) {
-              elapsedTime += (performance.now() - startTime) / 1000;
-            } else {
-              startTime = performance.now();
-              draw();
-            }
-        },
-        () => {
-            controlFactory.visible = !controlFactory.visible;
-        }
-    );
-
-    window.addEventListener("resize", resize);
-    draw();
+  window.addEventListener("resize", resize);
+  draw();
 }
 
 export default main;
 
 // TODO:
 
-// Ensure that UI parameters does not affect performance (e.g. since we use 3d context)
 // Expose particle fade time as parameter
 // Toggle trail
 // Fix or remove clear
-// Toggle performance mode
 // Store parameters
 // Random seed
 // Make vector field less jittery
