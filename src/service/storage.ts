f181f83730960480cb1735df28858c65c1025f6f
Daniel Skantze <daniel@skantze.org>
2025-10-23 11:13:48 +0200

    Complete local blob store implementation.

--- src/service/storage.ts
+++ src/service/storage.ts
@@ -10,8 +10,28 @@ export type Store<T> = {
   load: () => T[]
 }
 
-export type KVStore = {
-  put<T>(id: string, item: T): Promise<void>
-  get<T>(id: string): Promise<T>
-  has(id: string): Promise<boolean>
+export interface KeyedBlobItem {
+  id: string;
+}
+
+export interface BlobItemMetadata extends KeyedBlobItem {
+  name: string,
+  type: string,
+  addedAt: string,
+  size: number,
+}
+
+export interface BlobItemData extends KeyedBlobItem {
+  blob: any
+}
+
+export interface BlobItem extends BlobItemData, BlobItemMetadata {
+
+};
+
+export type BlobStore = {
+  list(type?: string): Promise<BlobItemMetadata[]>;
+  put(item: BlobItem): Promise<void>;
+  get(id: string): Promise<BlobItem | undefined>;
+  has(id: string): Promise<boolean>;
 }
